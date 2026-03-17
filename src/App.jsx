24c855e4d4366c7bf704a9d32b128d6d8b83bd4d
gialli24 <a.gialli2409@gmail.com>
2026-03-17 12:10:34 +0100

    display actors list on page  loading retriving data from provided endpoint

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,40 @@
+import { act, useEffect, useState } from 'react';
 import './App.css'
 
 function App() {
 
+  const endpoint = "https://lanciweb.github.io/demo/api/actors/";
+
+  const [actors, setActors] = useState([]);
+
+  /**
+   * 
+   * @param {string} endpoint 
+   */
+  function fetchActors(endpoint) {
+    fetch(endpoint)
+      .then(response => response.json())
+      .then(data => {
+        setActors(data);
+      })
+      .catch(error => console.log("Errore: " + error))
+  }
+
+  useEffect(() => {
+    fetchActors(endpoint);
+  }, [])
+
   return (
     <>
+      <main id="app-main">
+        <ul>
+          {
+            actors.map((actor, i) => (
+              <li key={i}>{actor.name}</li>
+            ))
+          }
+        </ul>
+      </main>
     </>
   )
 }
